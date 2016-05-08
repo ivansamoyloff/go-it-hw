3335a6f62344557d300e8d99b51a69fab6bd3f17
ivans <ivan.samoyloff@gmail.com>
2016-05-08 21:31:59 +0300

    fixed js hw 3-4

--- js2/script/script.js
+++ js2/script/script.js
@@ -10,16 +10,19 @@ var MyTest = {
     ],
 
     createTest: function(questions, answers){
-        var container = document.querySelector('body');
-        container.innerHTML += '<h2>'+'Тест по программированию'+'</h2>'+'</n>'+'<form>';
+        var form = document.querySelector('body');
+        form.innerHTML += '<h2>'+'Тест по программированию'+'</h2>'+ '<br>' +'<form>'+'<form>';
+
+        var container = document.querySelector('form');
+
         for (var i = 0; i<questions.length; i++){
-            container.innerHTML += '<ul>' + questions[i] + '</ul>';
+            container.innerHTML += questions[i] +'<ul>'+'</ul>';
             var ul = document.querySelectorAll('ul');
             for (var j =0; j<answers.length; j++){
-                ul[i].innerHTML += '<li>'+'<label>'+'<input type="radio">'+answers[i][j]+'</input>'+'</label>'+'</li>';
+                ul[i].innerHTML += '<li>'+'<label>'+answers[i][j]+'<input type="radio" name="'+[i]+'">'+'</input>'+'</label>'+'</li>';
             }
         }
-        container.innerHTML += '<button class="btn btn-info">' + 'Проверить мои результаты' + '</button>' + '\n' + '</form>';
+        container.innerHTML += '<button class="btn btn-info">' + 'Проверить мои результаты' + '</button>';
         var li = document.querySelectorAll('li');
         for(i = 0; i <li.length; i++) {
             li[i].className = 'list';}
